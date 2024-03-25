68974db7f53a83a7d7b002b1937ff6b200c60960
yassib1 <86356206+yibork@users.noreply.github.com>
2024-03-25 15:01:08 +0000

    update

---
+++ fleetmanagementclient/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [message, setMessage] = useState('Welcome to our React app!');
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button onClick={() => setMessage('Thanks for clicking!')}>
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default Home;
